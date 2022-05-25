d05a2624c5e04c10ca94fcd2f2e9d4e0b9ce4f33
Christopher <leechrs1@gmail.com>
2022-05-25 18:38:53 -0500

    Add heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const { notes } = require('./data/notes')
+
+const express = require('express');
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
